c8cb79364e112877ec49929df2c8a989e0926f99
Jack Pickett <jackpicket1997@gmail.com>
2018-12-18 02:19:16 +0000

    Initial commit for drinks diary web project

---
+++ WebAPI/app.js
@@ -0,0 +1,21 @@
+﻿// https://github.com/focusaurus/express_code_structure
+
+'use strict';
+var express = require('express');
+var config = require('./config');
+
+var app = express();
+
+
+log.info('Starting Drinks Diary API Server...')
+app.listen(config.express.port, config.express.ip, function (error) {
+    if (error) {
+        log.error('Unable to listen for connections', error)
+        process.exit(10)
+    }
+    log.info('express is listening on http://' +
+        config.express.ip + ':' + config.express.port)
+})
+
+// Export for testing
+module.exports = app
---
+++ WebAPI/config.js
@@ -0,0 +1,19 @@
+﻿// https://github.com/focusaurus/express_code_structure
+
+var config = module.exports
+var PRODUCTION = process.env.NODE_ENV === 'production'
+
+config.express = {
+    port: process.env.EXPRESS_PORT || 3000,
+    ip: '127.0.0.1'
+}
+
+config.mongodb = {
+    port: process.env.MONGODB_PORT || 27017,
+    host: process.env.MONGODB_HOST || 'localhost'
+}
+
+if (PRODUCTION) {
+    // for example
+    config.express.ip = '0.0.0.0'
+}
